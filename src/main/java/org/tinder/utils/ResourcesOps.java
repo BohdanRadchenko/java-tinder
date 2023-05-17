bce811b788cae8a7b93ae72b4fea9b3fea6d132f
Bohdan <radchenkobs@gmail.com>
2023-05-17 16:14:09 +0300

    refactor substring by os method

--- src/main/java/org/tinder/utils/ResourcesOps.java
+++ src/main/java/org/tinder/utils/ResourcesOps.java
@@ -5,25 +5,19 @@ import java.util.Objects;
 
 public class ResourcesOps {
 
-    private static String dirToOsType(String directory) {
-        if (OsUtil.isWindows()) return directory;
-        return directory.startsWith("/") ? directory.substring(1) : directory;
-    }
-
-    private static String pathToOsType(String path) {
-        if (!OsUtil.isWindows()) return path;
-        return path.startsWith("/") ? path.substring(1) : path;
+    private static String substringFirst(String string, boolean condition) {
+        return condition && string.startsWith("/") ? string.substring(1) : string;
     }
 
     public static String dir(String directory) {
-        directory = dirToOsType(directory);
+        directory = substringFirst(directory, !OsUtil.isWindows());
         try {
             String path = Objects.requireNonNull(ResourcesOps.class
                             .getClassLoader()
                             .getResource(directory))
                     .toURI()
                     .getPath();
-            return pathToOsType(path);
+            return substringFirst(path, OsUtil.isWindows());
         } catch (URISyntaxException e) {
             throw new RuntimeException(String.format("Requested path `%s`not found", directory), e);
         }
